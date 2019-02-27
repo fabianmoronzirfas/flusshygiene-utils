803fb27bdb9f7c9e41577258b2575d6713f7c272
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2019-02-27 10:59:01 +0100

    test(protocool): add test for strings passed in

--- __tests__/protocool.test.ts
+++ __tests__/protocool.test.ts
@@ -1,6 +1,6 @@
 import protocoolMessage from '../src/lib/protocool';
 
-const defaultObjStr = '{"type":"default"}';
+const defaultObjStr = '{"type":"DEFAULT"}';
 describe('testing protocool', () => {
   test('should return default string', () => {
     expect(protocoolMessage()).toBe(defaultObjStr);
@@ -16,5 +16,11 @@ describe('testing protocool', () => {
   test('should return default object string even if empty', () => {
     const obj = {};
     expect(protocoolMessage(obj)).toBe(defaultObjStr);
+  });
+  test('should wrap a string into the  default object string', () => {
+    const str = 'foo';
+    const res = JSON.parse(defaultObjStr);
+    res.payload = str;
+    expect(protocoolMessage(str)).toBe(JSON.stringify(res));
   });
 });
